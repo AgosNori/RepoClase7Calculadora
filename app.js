c5f3f8d0fe72f5c79d06dfed89ed609f3dcf6e21
AgostinaN <agosnoriega2018@gmail.com>
2023-04-18 20:22:10 -0300

    ahora funciona el archivo list

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-const fs = require ('fs')
-let data = fs.readFileSync("./data.json", "utf-8")
-let dataParseada = JSON.parse(data)
-console.log(data)
-console.log('-------------------------------')
-console.log(dataParseada)
+//const fs = require ('fs')
+//let data = fs.readFileSync("./data.json", "utf-8")
+//let dataParseada = JSON.parse(data)
+//console.log(data[0].titulo)
+//console.log('-------------------------------')
+//console.log(dataParseada[0].titulo)
+
+const {list }= require("./funciones/list");
+console.log(list())
